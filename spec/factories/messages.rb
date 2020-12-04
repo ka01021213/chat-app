c7807216b69e03a093d7bd51afc4bd6f51603c9f
ka01021213 <rockpanchi05@gmail.com>
2020-12-04 19:02:44 +0900

    各モデルのテスト

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
     association :user
     association :room
 
-    after(:build) do |item|
-      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
   end
 end
